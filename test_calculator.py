f9eceb57b2acd652e8a36902919af15949bd58a8
mathieuboc <mathieu.bocquel@esme.fr>
2024-04-12 15:21:21 +0200

    add test for addition, substraction and division

---
+++ test_calculator.py
@@ -0,0 +1,12 @@
+from calculator import addition
+from calculator import substraction
+from calculator import division
+
+def test_addition():
+	assert addition(1,1) == 2
+	
+def test_substraction():
+	assert substraction(2,1) == 1
+	
+def test_division():
+	assert division(2,1) == 2
